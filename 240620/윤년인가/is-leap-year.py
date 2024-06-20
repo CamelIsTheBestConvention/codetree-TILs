5a275fe03da1b99126a4a4e400d6283e8a693746
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-20 23:01:56 +0900

    Title: 윤년인가, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/윤년인가/is-leap-year.py
+++ 240620/윤년인가/is-leap-year.py
@@ -1,8 +1,8 @@
 a = int(input())
 
-if a % 4 == 0:
-    print("true")
-elif a % 100 == 0 and a % 400 != 0:
+if a % 100 == 0 and a % 400 != 0:
     print("false") 
+elif a % 4 == 0:
+    print("true")
 else:
     print("false")
