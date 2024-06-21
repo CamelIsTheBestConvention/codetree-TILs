495bd38ddd29098fe4e531a5715e3504ebc3c6db
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-21 22:15:06 +0900

    Title: 369 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240621/369 게임/369-game.py
+++ 240621/369 게임/369-game.py
@@ -1,7 +1,7 @@
 a = int(input())
 
 for i in range(1, a+1):
-    if i % 3 == 0 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+    if i % 3 == 0 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9 or i // 10 == 3:
         print(0, end=' ')
     else:
         print(i, end=' ')
