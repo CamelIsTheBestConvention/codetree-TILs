3a4d07d178e194dd2df68f6256b86b5c067a4149
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-24 15:30:32 +0900

    Title: ab 사이에 없는 c, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240624/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+cnt = a
+
+for i in range(a, b+1):
+    if cnt == b:
+        print("NO")
+
+    if c % i == 0:
+        print("YES")
+        break
+    else:
+        cnt += 1
+        continue
