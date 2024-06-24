995e874b1de86abca71066fb288841640d376faf
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-24 22:19:22 +0900

    Title: 별표 출력하기 4, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240624/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+for i in range(a):
+    for j in range(a, i, -1):
+        print('*', end=' ')
+    print()
+
+for i in range(a-1):
+    for j in range(i+2):
+        print('*', end=' ')
+    print()
