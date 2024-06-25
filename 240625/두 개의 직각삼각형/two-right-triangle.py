17b2a4885aaea6437d94aa6d08800c05f9ca5d32
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-25 21:48:43 +0900

    Title: 두 개의 직각삼각형, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240625/두 개의 직각삼각형/two-right-triangle.py
+++ 240625/두 개의 직각삼각형/two-right-triangle.py
@@ -1,5 +1,16 @@
 a = int(input())
 
 for i in range(a):
-    for j in range(a, 0, -1):
+    for j in range(a, i, -1):
         print('*', end='')
+    if i > 0:
+        for k in range(i):
+            print(' ', end='')
+
+    if i > 0:
+        for k in range(i):
+            print(' ', end='')
+    for j in range(a, i, -1):
+        print('*', end='')
+
+    print()
