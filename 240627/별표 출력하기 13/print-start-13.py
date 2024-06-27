7ff670d5a016d265823b3c59776453c01554e6d7
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-06-27 16:42:39 +0900

    Title: 별표 출력하기 13, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/별표 출력하기 13/print-start-13.py
+++ 240627/별표 출력하기 13/print-start-13.py
@@ -1,17 +1,9 @@
-def print_stars(n):
-    # 첫 번째 줄
-    print('* ' * n)
-    
-    # 중간 부분
-    for i in range(1, n):
-        print('* ' * i)
-        print('* ' * (n - i))
-    
-    # 마지막 줄
-    print('* ' * n)
-
-# 사용자로부터 정수 입력 받기
 n = int(input())
 
-# 별표 출력하기
-print_stars(n)
+print('* ' * n)
+    
+for i in range(1, n):
+    print('* ' * i)
+    print('* ' * (n - i))
+    
+print('* ' * n)
