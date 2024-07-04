1d1283feef9e14c101d5d419968de0aef48e4946
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-07-04 19:34:44 +0900

    Title: 100 도달하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240704/100 도달하기/reach-100.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = [1, n]
+i = 2
+
+print(arr[0], arr[1], end=' ')
+
+while True:
+    arr.append(arr[i-2] + arr[i-1])
+    print(arr[i], end=' ')
+    
+    if arr[i] >= 100:
+        break
+    i += 1
