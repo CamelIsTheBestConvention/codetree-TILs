4e9a351b6227348979dd0308d2b85ce5468d2a57
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-07-05 23:41:38 +0900

    Title: 점수대 파악하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+arr2 = [0] * 10
+
+for i in range(len(arr)):
+    if arr[i] == 0:
+        break
+        
+    score = arr[i] // 10
+
+    arr2[score-1] += 1
+
+for i in range(len(arr2), 0, -1):
+    print("%d - %d" % (i*10, arr2[i-1]))
